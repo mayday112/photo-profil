52627b8217c7891a9785ce977b9a9a58d30c38cc
Maidi <mhumaidi437@gmail.com>
2025-01-29 11:49:56 +0800

    add route photo

--- routes/api.php
+++ routes/api.php
@@ -8,5 +8,5 @@ Route::get('/user', function (Request $request) {
     return $request->user();
 })->middleware('auth:sanctum');
 
-// Route::apiResource('photo', PhotoController::class);
+Route::apiResource('photo', PhotoController::class);
 Route::post('save-photo/{phone}', [PhotoController::class, 'storePhoto']);
